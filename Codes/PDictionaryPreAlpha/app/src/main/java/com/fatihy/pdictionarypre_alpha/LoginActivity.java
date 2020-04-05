433af8a61cc9256350bbac3ce75e824f68408685
fatihy101 <fatihy@yahoo.com>
2020-04-05 03:16:13 +0300

    Interfaces for login and sign in created.

--- Codes/PDictionaryPreAlpha/app/src/main/java/com/fatihy/pdictionarypre_alpha/LoginActivity.java
+++ Codes/PDictionaryPreAlpha/app/src/main/java/com/fatihy/pdictionarypre_alpha/LoginActivity.java
@@ -3,12 +3,17 @@ package com.fatihy.pdictionarypre_alpha;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
-
+EditText email, password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        email = findViewById(R.id.emailLogin);
+        password = findViewById(R.id.passwordLogin);
+
     }
 }
